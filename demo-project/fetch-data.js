81b20b651f4a3be2e36f8a94ca726b9a90d34cfd
isajjadali <sajjad.ali5112@gmail.com>
2020-04-03 22:03:01 +0500

    demo-project completed

--- demo-project/fetch-data.js
+++ demo-project/fetch-data.js
@@ -11,23 +11,61 @@ getData()
         students.forEach(student => {
 
             student.subjects.forEach(subject => {
-            
+
                 let values = Object.values(subject.report.attendence);
 
                 subject.report.attendenceAverage = (values.reduce((acc, element) => {
                     return acc + element;
                 }, 0) / (values.length * 100)) * 100;
 
-            })
-        })
+                let finalPercentage = ((subject.report.attendenceAverage + subject.report.marks + subject.report.classParticipation) / 300) * 100;
+                subject.report.finalPercentage = finalPercentage;
+                if (finalPercentage < 50) {
+                    subject.report.progress = 'fail';
+                }
+                else if (finalPercentage < 60) {
+                    subject.report.progress = 'low';
+                }
+                else if (finalPercentage < 80) {
+                    subject.report.progress = 'average';
+                }
+                else {
+                    subject.report.progress = 'great';
+                }
+            });
+        });
 
-        console.log('--------------------------------------->>>>>');
-        console.log(JSON.stringify(students[0], null, 2));
-        console.log('--------------------------------------->>>>>');
         let teacher = filteredData();
 
-    })
+        const resultAgainstTeacherId1 = getStudentsAgainstTeacherId(1, students, 'fail');
+        const resultAgainstTeacherId2 = getStudentsAgainstTeacherId(2, students, 'fail');
 
+        console.log('With respect to teacherId ', resultAgainstTeacherId2.filter(student => student.isFailed).length);
+
+        console.log('With respect to subjects ', resultAgainstTeacherId2.reduce((acc, element) => {
+            acc += element.failedStudentCount;
+            return acc;
+        }, 0));
+
+    });
+
+
+function getStudentsAgainstTeacherId(teacherId = '', students, progress = '') {
+    const tempStudents = JSON.parse(JSON.stringify(students));
+
+    tempStudents.forEach(student => {
+        student.isFailed = student.subjects
+            .filter(subject => subject.teacherId === teacherId)
+            .some(subject => subject.report.progress === progress);
+    });
+
+    tempStudents.forEach(student => {
+        student.failedStudentCount = (student.subjects
+            .filter(subject => subject.teacherId === teacherId && subject.report.progress === progress)).length;
+    });
+
+    return tempStudents;
+}
 
     // Conditons for progress
     // fail if less than 50
